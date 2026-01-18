775e1aea3c9b306a57d62f17232b64eb4cb2fece
S0415_จารุภูมิ AI <tatang.mcw@gmail.com>
2026-01-18 12:43:54 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import pickle
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -45,4 +46,5 @@ def predict():
     })
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port)
